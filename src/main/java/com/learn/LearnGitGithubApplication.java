a3760d4c80a117b888f496be3458deee11b8db76
tom02_glb <learnGit02_glb@163.com>
2020-02-04 18:06:03 +0800

    第一次提交项目learn_git_github

---
+++ src/main/java/com/learn/LearnGitGithubApplication.java
@@ -0,0 +1,13 @@
+package com.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnGitGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnGitGithubApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/learn/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.learn.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author 20992
+ * @Date 2020/2/4
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("info")
+    public String info() {
+        return "Hello SpringBoot Info";
+    }
+}
